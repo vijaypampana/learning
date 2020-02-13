b25b4af34f79bc30eb8d2c4a00ffad8f50c904de
Vijay Kumar Pampana <vijaypampana@gmail.com>
2020-02-13 07:31:35 -0500

    baseline

--- src/main/java/learning/BDD/utilities/api/HTMLSteps.groovy
+++ src/main/java/learning/BDD/utilities/api/HTMLSteps.groovy
@@ -1,4 +1,96 @@
 package learning.BDD.utilities.api
 
+import cucumber.api.DataTable
+import cucumber.api.java.en.Given
+import learning.BDD.utilities.Context
+import learning.BDD.utilities.DataUtil
+import org.apache.commons.lang3.BooleanUtils
+
+import static org.hamcrest.Matchers.equalTo
+import static org.hamcrest.Matchers.not
+
 class HTMLSteps {
+
+    @Given("^I set API HTML root as \"(.*)\" and verify response using below table\$")
+    void setRootPath(String sRootPath, DataTable table) {
+        setRootPath(sRootPath)
+        verifyHTMLUsingTable(table)
+    }
+
+    @Given("^I set API HTML root as \"(.*)\"\$")
+    void setRootPath(String sRootPath) {
+        Context.getInstance().getValidatableResponse().rootPath(sRootPath)
+    }
+
+    @Given("^I verify API HTML response using below table\$")
+    void verifyHTMLUsingTable(DataTable table) {
+        table.raw().each { row ->
+            if(row.size()==3) {
+                verifyHTMLObjectEqualTo(row.get(0), row.get(1), row.get(3))
+            } else {
+                verifyHTMLObjectEqualTo(row.get(0), row.get(1))
+            }
+        }
+    }
+
+    verifyHTMLObjectEqualTo(String sQuery, String sExpectedValue, String sType) {
+        try {
+            sQuery = Context.getInstance().getData(sQuery)
+            sExpectedValue = Context.getInstance().getData(sExpectedValue)
+            Context.getInstance().getValidatableResponse().body(sQuery, equalTo(JSONSteps.getJsonType(sExpectedValue, sType)))
+        } catch (AssertionError e) {
+            Context.getInstance().getReports().stepFail(e.getMessage())
+        }
+    }
+
+    @Given("^I verify API HTML element \"(.*)\" equal to \"(.*)\"\$")
+    void verifyHTMLObjectEqualTo(String sQuery, String sExpectedValue) {
+        try {
+            sQuery = Context.getInstance().getData(sQuery)
+            sExpectedValue = Context.getInstance().getData(sExpectedValue)
+            if(sExpectedValue.equalsIgnoreCase("NULL")) {
+                Context.getInstance().getValidatableResponse().body(sQuery, equalTo(null))
+            } else if (sExpectedValue.equalsIgnoreCase("EMPTY") || sExpectedValue.equalsIgnoreCase("\"\"")) {
+                Context.getInstance().getValidatableResponse().body(sQuery+".isEmpty()", equalTo(true))
+            } else if (BooleanUtils.toBooleanObject(sExpectedValue) == Boolean.TRUE) {
+                Context.getInstance().getValidatableResponse().body(sQuery, equalTo(true))
+            } else if (BooleanUtils.toBooleanObject(sExpectedValue) == Boolean.FALSE) {
+                Context.getInstance().getValidatableResponse().body(sQuery, equalTo(false))
+            } else {
+                Context.getInstance().getValidatableResponse().body(sQuery, equalTo(sExpectedValue))
+            }
+        } catch (AssertionError e) {
+            Context.getInstance().getReports().stepFail(e.getMessage())
+        }
+    }
+
+    @Given("^I verify API HTML element \"(.*)\" not equal to \"(.*)\"\$")
+    void verifyHTMLObjectNotEqualTo(String sQuery, String sExpectedValue) {
+        try {
+            sQuery = Context.getInstance().getData(sQuery)
+            sExpectedValue = Context.getInstance().getData(sExpectedValue)
+            if(sExpectedValue.equalsIgnoreCase("NULL")) {
+                Context.getInstance().getValidatableResponse().body(sQuery, not(equalTo(null)))
+            } else if (sExpectedValue.equalsIgnoreCase("EMPTY") || sExpectedValue.equalsIgnoreCase("\"\"")) {
+                Context.getInstance().getValidatableResponse().body(sQuery+".isEmpty()", not(equalTo(true)))
+            } else if (BooleanUtils.toBooleanObject(sExpectedValue) == Boolean.TRUE) {
+                Context.getInstance().getValidatableResponse().body(sQuery, equalTo(false))
+            } else if (BooleanUtils.toBooleanObject(sExpectedValue) == Boolean.FALSE) {
+                Context.getInstance().getValidatableResponse().body(sQuery, equalTo(true))
+            } else {
+                Context.getInstance().getValidatableResponse().body(sQuery, not(equalTo(sExpectedValue)))
+            }
+        } catch (AssertionError e) {
+            Context.getInstance().getReports().stepFail(e.getMessage())
+        }
+    }
+
+    @Given("^I store API HTML element \"(.*)\" to \"(.*)\"\$")
+    void storeHTMLObject(String sQuery, String sMapKey) {
+        sQuery = Context.getInstance().getData(sQuery)
+        String sMapValue = Context.getInstance().getValidatableResponse().extract().body().htmlPath().getString(sQuery)
+        Context.getInstance().addData(sMapKey, sMapValue)
+    }
+
+
 }
--- src/main/java/learning/BDD/utilities/api/XMLSteps.groovy
+++ src/main/java/learning/BDD/utilities/api/XMLSteps.groovy
@@ -1,4 +1,98 @@
 package learning.BDD.utilities.api
 
+import cucumber.api.DataTable
+import cucumber.api.java.en.Given
+import learning.BDD.utilities.Context
+import learning.BDD.utilities.DataUtil
+import org.apache.commons.lang3.BooleanUtils
+
+import static org.hamcrest.Matchers.*
+
 class XMLSteps {
+
+    @Given("^I set API XML root as \"(.*)\" and verify response using below table\$")
+    void setRootPath(String sRootPath, DataTable table) {
+        setRootPath(sRootPath)
+        verifyXMLUsingTable(table)
+    }
+
+    @Given("^I set API XML root as \"(.*)\"\$")
+    void setRootPath(String sRootPath) {
+        Context.getInstance().getValidatableResponse().rootPath(sRootPath)
+    }
+
+    @Given("^I verify API XML response using below table\$")
+    void verifyXMLUsingTable(DataTable table) {
+        table.raw().each { row ->
+            if(row.size()==3) {
+                verifyXMLObjectEqualTo(row.get(0), row.get(1), row.get(3))
+            } else {
+                verifyXMLObjectEqualTo(row.get(0), row.get(1))
+            }
+        }
+    }
+
+    verifyXMLObjectEqualTo(String sQuery, String sExpectedValue, String sType) {
+        try {
+            sQuery = Context.getInstance().getData(sQuery)
+            sExpectedValue = Context.getInstance().getData(sExpectedValue)
+            Context.getInstance().getValidatableResponse().body(sQuery, equalTo(JSONSteps.getJsonType(sExpectedValue, sType)))
+        } catch (AssertionError e) {
+            Context.getInstance().getReports().stepFail(e.getMessage())
+        }
+    }
+
+    @Given("^I verify API XML element \"(.*)\" equal to \"(.*)\"\$")
+    void verifyXMLObjectEqualTo(String sQuery, String sExpectedValue) {
+        try {
+            sQuery = Context.getInstance().getData(sQuery)
+            sExpectedValue = Context.getInstance().getData(sExpectedValue)
+            if(sExpectedValue.equalsIgnoreCase("NULL")) {
+                Context.getInstance().getValidatableResponse().body(sQuery, equalTo(null))
+            } else if (sExpectedValue.equalsIgnoreCase("EMPTY") || sExpectedValue.equalsIgnoreCase("\"\"")) {
+                Context.getInstance().getValidatableResponse().body(sQuery+".isEmpty()", equalTo(true))
+            } else if (BooleanUtils.toBooleanObject(sExpectedValue) == Boolean.TRUE) {
+                Context.getInstance().getValidatableResponse().body(sQuery, equalTo(true))
+            } else if (BooleanUtils.toBooleanObject(sExpectedValue) == Boolean.FALSE) {
+                Context.getInstance().getValidatableResponse().body(sQuery, equalTo(false))
+            } else {
+                Context.getInstance().getValidatableResponse().body(sQuery, equalTo(sExpectedValue))
+            }
+        } catch (AssertionError e) {
+            Context.getInstance().getReports().stepFail(e.getMessage())
+        }
+    }
+
+    @Given("^I verify API XML element \"(.*)\" not equal to \"(.*)\"\$")
+    void verifyXMLObjectNotEqualTo(String sQuery, String sExpectedValue) {
+        try {
+            sQuery = Context.getInstance().getData(sQuery)
+            sExpectedValue = Context.getInstance().getData(sExpectedValue)
+            if(sExpectedValue.equalsIgnoreCase("NULL")) {
+                Context.getInstance().getValidatableResponse().body(sQuery, not(equalTo(null)))
+            } else if (sExpectedValue.equalsIgnoreCase("EMPTY") || sExpectedValue.equalsIgnoreCase("\"\"")) {
+                Context.getInstance().getValidatableResponse().body(sQuery+".isEmpty()", not(equalTo(true)))
+            } else if (BooleanUtils.toBooleanObject(sExpectedValue) == Boolean.TRUE) {
+                Context.getInstance().getValidatableResponse().body(sQuery, equalTo(false))
+            } else if (BooleanUtils.toBooleanObject(sExpectedValue) == Boolean.FALSE) {
+                Context.getInstance().getValidatableResponse().body(sQuery, equalTo(true))
+            } else {
+                Context.getInstance().getValidatableResponse().body(sQuery, not(equalTo(sExpectedValue)))
+            }
+        } catch (AssertionError e) {
+            Context.getInstance().getReports().stepFail(e.getMessage())
+        }
+    }
+
+    @Given("^I store API XML element \"(.*)\" to \"(.*)\"\$")
+    void storeXMLObject(String sQuery, String sMapKey) {
+        sQuery = Context.getInstance().getData(sQuery)
+        String sMapValue = Context.getInstance().getValidatableResponse().extract().body().xmlPath().getString(sQuery)
+        Context.getInstance().addData(sMapKey, sMapValue)
+    }
+
+    static <T> List<T> getListByResponseKey(String sKey, String sQuery) {
+        return DataUtil.retrieveValidatableResponse(sKey).extract().body().xmlPath().getString(sQuery)
+    }
+
 }
