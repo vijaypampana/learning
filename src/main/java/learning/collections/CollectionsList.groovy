2b92ea04cc06cfa3daa0841dc6834b3e6b25f24e
Vijay Kumar Pampana <vijaypampana@gmail.com>
2020-02-22 16:41:58 -0500

    baseline

---
+++ src/main/java/learning/collections/CollectionsList.groovy
@@ -0,0 +1,33 @@
+package learning.collections
+
+import org.testng.annotations.Test
+
+class CollectionsList {
+
+    @Test
+    void test() {
+
+        List<String> namesA = Arrays.asList("test", "Vijay", "Deepika", "Sanvika")
+        List<String> namesAL = new ArrayList<>()
+        List<String> namesLL = new LinkedList<>()
+
+        println("ArrayList Size : ${namesA.size()}")
+        println("Retrieving List Second data based on index : ${namesA.get(1)}")
+        namesA.set(0, "testindex0")
+        println("Printing the list namesA : $namesA")
+        namesAL.add("Violin")
+        namesAL.addAll("Piano", "Drum", "Guitar")
+        println("Printing the Array List element ${namesAL.each {val -> print(val)}}")
+        //namesAL.sort()
+        Collections.sort(namesAL)
+        namesAL.removeIf{val -> val.startsWith("Dr")}
+        println(namesAL)
+        Collections.sort(namesAL, Collections.reverseOrder())
+        println(namesAL)
+
+        namesLL.addAll("Rahul", "Hari", "Mohan")
+        println(namesLL)
+
+    }
+
+}
